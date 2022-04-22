99f6db9a3006e527bc345432969eeab1365f3f79
Alaa Atef <alaa00x0@gmail.com>
2022-04-22 19:07:54 +0200

    changes

--- js/app.js
+++ js/app.js
@@ -2,10 +2,16 @@ $('.hamburger').click(function () {
   $('.navbar-nav').addClass('active');
   $('.hamburger').hide();
   $('.close').show();
+  $('body').addClass('fixed-position');
 });
 
 $('.close').click(function () {
   $('.navbar-nav').removeClass('active');
   $('.close').hide();
   $('.hamburger').show();
+  $('body').removeClass('fixed-position');
 });
+
+// $('body').click(function () {
+//   $('body').removeClass('fixed-position');
+// });
